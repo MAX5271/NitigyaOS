3c158f0585962ce72be116cb702ddcb66ea1522d
Nitigya Chandel <nitigyasingh27@gmail.com>
2026-09-07 15:49:25 +0530

    Add: GDT Segmentation

--- src/loader.s
+++ src/loader.s
@@ -12,12 +12,65 @@ dd CHECKSUM
 global loader
 extern kernel_main
 
+; -------------------------
+; GDT
+; -------------------------
+
+gdt_start:
+
+    ; Null descriptor
+    dq 0
+
+    ; Kernel Code
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x9A
+    db 0xCF
+    db 0x00
+
+    ; Kernel Data
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x92
+    db 0xCF
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
 loader:
+    lgdt [gdt_descriptor]
+
+    ; Load Kernel Code segment
+    jmp 0x08:reload_segments
+
+reload_segments:
+    ; Load Kernel Data segment
+    mov ax, 0x10
+
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; Set up stack
     mov esp, stack_top
+
+    ; Enter C
     call kernel_main
 
 .loop:
     jmp .loop
+
+; -------------------------
+; Port I/O
+; -------------------------
 
 global outb
 
@@ -26,6 +79,10 @@ outb:
     mov dx, [esp + 4]
     out dx, al
     ret
+
+; -------------------------
+; Stack
+; -------------------------
 
 section .bss
 
